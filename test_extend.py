74064410d3a14686f4e8b43e78d36f23b5fb3b00
sixtsa01 <32494159+sixtsa01@users.noreply.github.com>
2019-10-17 21:06:02 -0500

    Update test_extend.py

--- test_extend.py
+++ test_extend.py
@@ -1,7 +1,7 @@
 import pandas as pd
-from refrom import Relation
+from reframe import Relation
 
-def test_extend():
+def test_extend1():
   data_real = {"name": ["Joe", "Jim", "Sam"], "salary": [60,40,20], "salary_new": [80,60,40]}
   data_expected = {"name": ["Joe", "Jim", "Sam"], "salary_diff": [20,20,20]
   df = pd.DataFrame(data=data_real)
@@ -10,4 +10,24 @@ def test_extend():
   r_expected = Relation(df_expected)
   r = r.extend("salary_diff", r.salary_new - r.salary)
   assert r.equals(r_expected)
-  
+
+                   
+def test_extend2():
+  data_real = {"name": ["Sandy", "Deb", "Alex"], "age": [18,21,44], "age_real": [22,23,50]}
+  data_expected = {"name": ["Sandy", "Deb", "Alex"], "age_diff": [4,2,6]
+  df = pd.DataFrame(data=data_real)
+  df_expected = pd.DataFrame(data=data_expected)
+  r = Relation(df)
+  r_expected = Relation(df_expected)
+  r = r.extend("age_diff", r.age_real - r.age)
+  assert r.equals(r_expected)
+
+def test_extend3():
+  data_real = {"name": ["Karissa", "Syd", "Brooke"], "gpa": [3,3,2], "gpa_real": [4,4,3]}
+  data_expected = {"name": ["Karissa", "Syd", "Brooke"], "gpa_diff": [1,1,1]
+  df = pd.DataFrame(data=data_real)
+  df_expected = pd.DataFrame(data=data_expected)
+  r = Relation(df)
+  r_expected = Relation(df_expected)
+  r = r.extend("gpa_diff", r.gpa_real - r.gpa)
+  assert r.equals(r_expected)
