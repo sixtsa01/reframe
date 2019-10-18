1f7e0a5e8613570ae24f63ef5de6d3bbb377183b
sixtsa01 <32494159+sixtsa01@users.noreply.github.com>
2019-10-17 20:39:31 -0500

    Create test_union.py

---
+++ test_union.py
@@ -0,0 +1,12 @@
+import sys
+sys.path.insert(1, '../')
+import pytest
+import sys
+import pandas as pd
+from reframe import Relation
+
+def test_union():
+  df = Relation("../country.csv")
+  df_region = df.query("region == 'North America'")
+  df_union = df_region.union(df_region)
+  assert len(df_region) == len(df_union)
