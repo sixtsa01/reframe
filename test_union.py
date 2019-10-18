291fb3782bf4770d0706d4383430df60ea64dccc
sixtsa01 <32494159+sixtsa01@users.noreply.github.com>
2019-10-17 21:22:39 -0500

    Update test_union.py

--- test_union.py
+++ test_union.py
@@ -13,6 +13,12 @@ def test_union1():
 
 def test_union2():
   df = Relation("../country.csv")
-  df_region = df.query("region == 'North America'")
-  df_union = df_region.union(df_region)
-  assert len(df_region) == len(df_union)
+  df_indepyear = df.query("indepyear == 1901")
+  df_union = df_indepyear.union(df_indepyear)
+  assert len(df_indepyear) == len(df_union)
+  
+def test_union3():
+  df = Relation("../country.csv")
+  df_continent = df.query("continent == 'Asia'")
+  df_union = df_continent.union(df_continent)
+  assert len(df_continent) == len(df_union)
