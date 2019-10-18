24372fcc55c85bf6a246c69d463c8e0fe6d4bbd8
sixtsa01 <32494159+sixtsa01@users.noreply.github.com>
2019-10-17 21:12:02 -0500

    Update test_extend.py

--- test_extend.py
+++ test_extend.py
@@ -3,7 +3,7 @@ from reframe import Relation
 
 def test_extend1():
   data_real = {"name": ["Joe", "Jim", "Sam"], "salary": [60,40,20], "salary_new": [80,60,40]}
-  data_expected = {"name": ["Joe", "Jim", "Sam"], "salary_diff": [20,20,20]
+  data_expected = {"name": ["Joe", "Jim", "Sam"], "salary": [60,40,20], "salary_new": [80,60,40], "salary_diff": [20,20,20]}
   df = pd.DataFrame(data=data_real)
   df_expected = pd.DataFrame(data=data_expected)
   r = Relation(df)
@@ -14,7 +14,7 @@ def test_extend1():
                    
 def test_extend2():
   data_real = {"name": ["Sandy", "Deb", "Alex"], "age": [18,21,44], "age_real": [22,23,50]}
-  data_expected = {"name": ["Sandy", "Deb", "Alex"], "age_diff": [4,2,6]
+  data_expected = {"name": ["Sandy", "Deb", "Alex"], "age": [18,21,44], "age_real": [22,23,50], "age_diff": [4,2,6]}
   df = pd.DataFrame(data=data_real)
   df_expected = pd.DataFrame(data=data_expected)
   r = Relation(df)
@@ -24,7 +24,7 @@ def test_extend2():
 
 def test_extend3():
   data_real = {"name": ["Karissa", "Syd", "Brooke"], "gpa": [3,3,2], "gpa_real": [4,4,3]}
-  data_expected = {"name": ["Karissa", "Syd", "Brooke"], "gpa_diff": [1,1,1]
+  data_expected = {"name": ["Karissa", "Syd", "Brooke"], "gpa": [3,3,2], "gpa_real": [4,4,3], "gpa_diff": [1,1,1]}
   df = pd.DataFrame(data=data_real)
   df_expected = pd.DataFrame(data=data_expected)
   r = Relation(df)
